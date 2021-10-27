aadf75c16b54709b2ce3d4cb883e39f93260615d
Pavel <pavelm5@mail.ru>
2021-10-27 09:06:15 +0300

    этап _Формы и пользователи -незаверш.

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 
+from webapp.forms import LoginForm 
 from webapp.python_org_news import get_python_news
 from webapp.weather import weather_by_city
 from webapp.model import db, News
@@ -8,11 +9,19 @@ def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
     db.init_app(app)
+
     @app.route('/')
     def index():
         title = 'Новости Python'
         weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
         news_list = News.query.order_by(News.published.desc()).all()
         return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
+
+    @app.route('/login')
+    def login():
+        title = 'Авторизация'
+        login_form = LoginForm()
+        return render_template('login.html', page_title=title,  form=login_form)
+
 
     return app
